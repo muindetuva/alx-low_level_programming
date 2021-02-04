969ae105caff24b063bea8bd3bb7af1b64ef9b3f
Alfred Tuva <muindetuva@gmail.com>
2021-02-04 12:38:24 +0000

    Remove word is from code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	else if (j == 0)
 		printf("Last digit of %d is %d and 0\n", n, j);
 	else if (j < 6 && j != 0)
-		printf("Last digit of %d is %d and is less than 6 and is not 0\n", n, j);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, j);
 
 	return (0);
 }
