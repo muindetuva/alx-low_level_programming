5bc27b213022a8c9e5beba7f0afeaad5bbcf9074
Alfred Tuva <muindetuva@gmail.com>
2021-02-09 16:08:56 +0000

    Add task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+  * print_sign - adds the sign of a number to the number
+  * @n: the number being checked
+  *
+  * Return: 1 when positive, 0 when equal to zero & -1 when negative
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
