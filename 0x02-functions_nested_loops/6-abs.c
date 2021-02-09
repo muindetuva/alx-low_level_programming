3c73dc1f022356a5d44babf1a48ae0fd678d7ad5
Alfred Tuva <muindetuva@gmail.com>
2021-02-09 16:44:11 +0000

    Remove Putchar

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -11,17 +11,16 @@ int _abs(int n)
 
 	if (n > 0)
 	{
-		_putchar(n);
+		return (n);
 	}
 	else if (n == 0)
 	{
-		_putchar(n);
+		return (n);
 	}
 	else if (n < 0)
 	{
 		n = n * -1;
-		_putchar(n);
+		return (n);
 	}
-	return (n);
 }
 
