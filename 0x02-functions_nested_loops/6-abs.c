7c66a43b65c99a1af357a28cd8e8ee4e839a03a4
Alfred Tuva <muindetuva@gmail.com>
2021-02-09 16:48:27 +0000

    Make return be at the end of the code

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -11,16 +11,16 @@ int _abs(int n)
 
 	if (n > 0)
 	{
-		return (n);
+		n = n;
 	}
 	else if (n == 0)
 	{
-		return (n);
+		n = n;
 	}
 	else if (n < 0)
 	{
 		n = n * -1;
-		return (n);
 	}
+	return (n);
 }
 
