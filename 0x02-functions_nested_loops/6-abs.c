6815573d7d9bedec942cea7411d1336caf192d69
Alfred Tuva <muindetuva@gmail.com>
2021-02-09 16:30:24 +0000

    Add task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+  * _abs - calculates absolute value of a number
+  * @n: the number being checked
+  *
+  * Return: 0 when succesfuly executed
+  */
+int _abs(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar(n);
+	}
+	else if (n == 0)
+	{
+		_putchar(n);
+	}
+	else if (n < 0)
+	{
+		n *= -1;
+		_putchar(n);
+	}
+	return (0);
+}
+
