c0493eaa2aacb9666ddd40fa2ec1c5b18f30f945
Alfred Tuva <muindetuva@gmail.com>
2021-02-10 13:39:53 +0000

    a

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -25,10 +25,11 @@ void print_to_98(int n)
 			}
 		}
 	}
-	else if(n > 98)
+	else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
-		{	if (i != 98)
+		{
+			if (i != 98)
 			{
 				printf("%d, ", i);
 			}
