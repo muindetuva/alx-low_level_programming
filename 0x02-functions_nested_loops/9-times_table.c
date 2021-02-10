804a47ac061dd1a6024a9884b62c3508b28f113d
Alfred Tuva <muindetuva@gmail.com>
2021-02-10 06:14:01 +0000

    Add task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+  * times_table - prints the 9 by 9 times table
+  *
+  * Return: void
+  */
+
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		k = 0;
+		for (j = 0; j < 10; j++)
+		{
+			_putchar('k' + '0');
+			while (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			k = k + i
+		}
+		_putchar('\n');
+	}
+}
