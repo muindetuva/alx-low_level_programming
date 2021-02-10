f034cc51c24b7ac1f7d41aefb2056df7103dc30f
Alfred Tuva <muindetuva@gmail.com>
2021-02-10 13:50:45 +0000

    Correct print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -29,7 +29,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			if (i != 98)
+			if (i = 98)
 			{
 				printf("%d, ", i);
 			}
@@ -41,7 +41,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		printf(n);
+		printf("%d", n);
 	}
 	printf("\n");
 }
