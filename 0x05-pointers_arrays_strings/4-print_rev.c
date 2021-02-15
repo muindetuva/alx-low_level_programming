8ccdf55b3e2ba069b94ba1cad130b09b6161d056
Alfred Tuva <muindetuva@gmail.com>
2021-02-15 20:05:31 +0000

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+  * print_rev - prints a string in reverse
+  * @s: string to reverse
+  *
+  * Return: void
+  */
+
+ void print_rev(char *s)
+{
+	int i, len = 0;
+
+	while (*s != '\0')
+		len++;
+
+	len = len - 1;
+	j = 0;
+	while (
+
+}
