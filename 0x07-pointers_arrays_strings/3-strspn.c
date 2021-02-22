e7f99aa8bc3e1c1e766ea315ccfce7935082428c
Alfred Tuva <muindetuva@gmail.com>
2021-02-22 19:12:09 +0000

    Add task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,15 +11,20 @@ unsigned int _strspn(char *s, char *accept)
 	int i, j;
 	unsigned int n = 0;
 
+	if (s == 0 || accept == 0)
+		return (n);
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == s[j] && s[i] != ' ')
+			if (s[i] == accept[j])
 			{
 				n++;
 				break;
 			}
+			if (accept[j + 1] == '\0' && s[i] != accept[j])
+				return (n);
 		}
 	}
 	return (n);
