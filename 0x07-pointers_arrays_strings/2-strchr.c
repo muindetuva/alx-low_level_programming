86ca8ba18da8a63aa9dd5e2eeea9e731418a6ed5
Alfred Tuva <muindetuva@gmail.com>
2021-02-22 15:46:49 +0000

    Add task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+/**
+  * *_strchr - locates a character in a string
+  * @s: The string to be checked
+  * @c: The character to be looked for
+  *
+  * Return: pointer to first occurrence of letter and NULL if not found
+  */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *z;
+
+	z = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			z = &s[i];
+			break;
+		}
+	}
+	return (z);
+}
