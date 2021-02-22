00efd8b53a4a5d415ee18a6054ceacca1ac1ab4e
Alfred Tuva <muindetuva@gmail.com>
2021-02-22 17:03:43 +0000

    Add task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+/**
+  * _strspn - returns length of specified substring
+  * @s: string being checked
+  * @accept: substring to be used
+  *
+  * Return: number of bytes in segment of s
+  */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	unsigned int n = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == s[j])
+			{
+				n++;
+				break;
+			}
+		}
+	}
+	return (n);
+}
