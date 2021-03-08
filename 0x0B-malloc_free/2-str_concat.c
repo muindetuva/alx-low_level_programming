4e2f15ef3fd9e38b4efb5ecdd23506a1c3d36487
Alfred Tuva <muindetuva@gmail.com>
2021-03-08 14:52:24 +0000

    Account for NULL strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,11 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, j, i, size;
 	char *new_str;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	len1 = 0;
 	len2 = 0;
 	while (s1[len1] != '\0')
