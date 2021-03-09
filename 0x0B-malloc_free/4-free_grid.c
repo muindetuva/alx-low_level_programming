fb5556daefb20cd04a03cd01c2614b0e51834ec0
Alfred Tuva <muindetuva@gmail.com>
2021-03-09 18:59:47 +0000

    Add task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+/**
+  * free_grid - frees memory allocated in a grid
+  * @grid: The grid to be freed
+  * @height: the height of the grid
+  *
+  */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
