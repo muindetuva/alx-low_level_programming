101a8b8ddf1ac561c44e7ec41a4630ad4fdf8079
Alfred Tuva <muindetuva@gmail.com>
2021-03-09 13:56:03 +0000

    Add else block to rest of code

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,10 +20,13 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 
-		printf("Age: %f\n", d->age);
+		if (d->age <= 0)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %f\n", d->age);
 
 		if (d->owner == NULL)
-			printf("Age: (nil)\n");
+			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", d->owner);
 	}
