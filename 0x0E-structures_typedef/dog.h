7797a4c6a36a8542a4acb74ab9a2e96c210c65a0
Alfred Tuva <muindetuva@gmail.com>
2021-03-09 12:49:21 +0000

    Add task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Characteristics of a dog
+ * @name: The dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
