675490921eb19912038227195167839eaaac77a1
Alfred Tuva <muindetuva@gmail.com>
2021-03-09 13:35:47 +0000

    Add new line after printing nill

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,15 +16,15 @@ void print_dog(struct dog *d)
 	else
 	{
 		if (d->name == NULL)
-			printf("NAME: (nil)");
+			printf("NAME: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
 		if (d->age == 0)
-			printf("Age: (nil)");
+			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
-			printf("Age: (nil)");
+			printf("Age: (nil)\n");
 		else
 			printf("Owner: %s\n", d->owner);
 	}
