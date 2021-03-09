9f433815e323ee66e1b06811fae0fedcf10b6570
Alfred Tuva <muindetuva@gmail.com>
2021-03-09 13:31:02 +0000

    Nest the rest of the code in an else statement

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,17 +10,22 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
+	{
 		return;
-	if (d->name == NULL)
-		printf("NAME: (nil)");
+	}
 	else
-		printf("Name: %s\n", d->name);
-	if (d->age == 0)
-		printf("Age: (nil)");
-	else
-		printf("Age: %f\n", d->age);
-	if (d->owner == NULL)
-		printf("Age: (nil)");
-	else
-		printf("Owner: %s\n", d->owner);
+	{
+		if (d->name == NULL)
+			printf("NAME: (nil)");
+		else
+			printf("Name: %s\n", d->name);
+		if (d->age == 0)
+			printf("Age: (nil)");
+		else
+			printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			printf("Age: (nil)");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
 }
