1026967ebfc4e25bc3316259ea591f47c512a05e
Alfred Tuva <muindetuva@gmail.com>
2021-03-10 09:45:44 +0000

    Account for NULL

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
   * print_name - Prints a name using the passed function
   * @name: Name to be printed
@@ -7,6 +9,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
 
