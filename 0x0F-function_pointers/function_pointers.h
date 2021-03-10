05403af4ed444d5e05082d93c94bf5d36e70a821
Alfred Tuva <muindetuva@gmail.com>
2021-03-10 09:37:20 +0000

    Add stdlib to header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-
+#include <stdlib.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
