cf135c563c9afe3ac5b1cacc2495db94a972ef7d
Alfred Tuva <muindetuva@gmail.com>
2021-03-11 10:05:24 +0000

    Remove space after separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(numbers_list, int));
 		if (separator != NULL && i != n - 1)
-			printf("%c ", *separator);
+			printf("%c", *separator);
 	}
 	va_end(numbers_list);
 	printf("\n");
