4beb7559196fe24285c8c599058f3ea9aabb3438
Alfred Tuva <muindetuva@gmail.com>
2021-03-11 10:13:18 +0000

    Use pointer instead of pointer of pointer

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(numbers_list, int));
 		if (separator != NULL && i != n - 1)
-			printf("%s ", *separator);
+			printf("%s ", separator);
 	}
 	va_end(numbers_list);
 	printf("\n");
