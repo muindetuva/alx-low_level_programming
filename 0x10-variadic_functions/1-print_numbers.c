32f0962342f5ec714d9dc1cd2717a4f4aacd6227
Alfred Tuva <muindetuva@gmail.com>
2021-03-11 10:30:13 +0000

    Account for n = 0

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,10 +12,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list numbers_list;
 	unsigned int i;
 
-	if (separator == NULL)
-		separator = "";
 	if (n == 0)
 		return;
+
+	if (separator == NULL)
+		separator = "";
 
 	va_start(numbers_list, n);
 
