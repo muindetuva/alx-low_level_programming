6a9aa6786a501939df679131ff24095072492cbb
Alfred Tuva <muindetuva@gmail.com>
2021-03-11 12:22:45 +0000

    Add semicolon

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -30,7 +30,7 @@ void print_all(const char * const format, ...)
 			case 's':
 				str = va_arg(strings, char *);
 				if (str == NULL)
-					str = "(nil)"
+					str = "(nil)";
 				printf("%s%s", sep, str);
 				break;
 		}
