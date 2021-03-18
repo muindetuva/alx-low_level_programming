95a278cfd7fde6635e4df66a591f39dea22241fe
Alfred Tuva <muindetuva@gmail.com>
2021-03-18 19:14:55 +0000

    Add advanced task 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * print_before - Print something before main is executed
+  *
+  */
+
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
