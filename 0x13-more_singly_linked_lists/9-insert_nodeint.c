cae6bc3d36235818f20a523721047782ec601b70
Alfred Tuva <muindetuva@gmail.com>
2021-03-23 10:26:36 +0000

    Revert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,18 +20,26 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
+	new->next = NULL;
 	current = *head;
+<<<<<<< HEAD
 	if (current == NULL)
 		return (NULL);
+=======
+>>>>>>> parent of 63bfabe... Acoount for 0
 	while (current != NULL)
 	{
 		if (idx == 0)
 		{
-			new->next = *head;
-			*head = new;
+			new->next = current;
+			current = new;
 			return (new);
 		}
+<<<<<<< HEAD
 		if (i == (idx - 1))
+=======
+		else if (i == (idx - 1))
+>>>>>>> parent of 63bfabe... Acoount for 0
 		{
 			new->next = current->next;
 			current->next  = new;
@@ -42,7 +50,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			current = current->next;
 			i++;
 		}
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 63bfabe... Acoount for 0
 	}
 	return (NULL);
 }
