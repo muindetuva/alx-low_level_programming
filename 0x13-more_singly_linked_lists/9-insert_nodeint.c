197cbd0b833ffd5919355beb7e7229a3f054f1c0
Alfred Tuva <muindetuva@gmail.com>
2021-03-23 10:22:10 +0000

    Check if current is NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new->n = n;
 	current = *head;
+	if (current == NULL)
+		return (NULL);
 	while (current != NULL)
 	{
 		if (idx == 0)
