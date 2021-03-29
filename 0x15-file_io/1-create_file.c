d753a0d8183e9b2537faa440956398400cd3f30d
Alfred Tuva <muindetuva@gmail.com>
2021-03-29 12:03:34 +0000

    Check if string exists

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	/*Find length of content*/
-	while (text_content[len])
+	while (text_content[len] && text_content)
 		len++;
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
@@ -27,8 +27,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 		check = write(fd, text_content, len);
-	else
-		check =  write(fd, "", len);
 
 	if (check == -1)
 		return (-1);
