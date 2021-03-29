0a8dc0a4f9c4bd960dd9be4c02f1b2d7fdf942dd
Alfred Tuva <muindetuva@gmail.com>
2021-03-29 11:04:21 +0000

    Add read only mode

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	size_t len = 0;
-	ssize_t check;
+	ssize_t check = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,16 +21,16 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[len])
 		len++;
 
-	fd = open(filename, O_CREAT | O_TRUNC, 0600);
+	fd = open(filename,O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 		check = write(fd, text_content, len);
 
-	close(fd);
 	if (check == -1)
 		return (-1);
 
+	close(fd);
 	return (1);
 }
