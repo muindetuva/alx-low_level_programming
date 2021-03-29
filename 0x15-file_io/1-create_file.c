608036def865058e470359a5244324fcea15ad81
Alfred Tuva <muindetuva@gmail.com>
2021-03-29 11:58:35 +0000

    Use symbolic constant instead of NUMBER

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[len])
 		len++;
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 		return (-1);
