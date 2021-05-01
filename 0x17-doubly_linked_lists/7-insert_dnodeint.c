deb64255335ec44d948541866ca21c9394535813
Alfred Tuva <muindetuva@gmail.com>
2021-05-01 16:47:48 +0000

    Account for position 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,6 +22,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->next = NULL;
 
 	before = *h;
+	if (idx == 0)
+	{
+		new_node->next = *h;
+		(*h)->prev = new_node;
+		(*h) = new_node;
+		return (new_node);
+	}
+
 
 	while (idx > 1)
 	{
