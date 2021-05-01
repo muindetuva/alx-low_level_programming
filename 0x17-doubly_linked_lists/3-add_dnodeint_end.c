339000f1c1f50eaec750e66d9c0975cf6771666f
Alfred Tuva <muindetuva@gmail.com>
2021-05-01 14:26:42 +0000

    Remove malloc for ptr

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,8 +13,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new_node, *ptr;
 
 	new_node = malloc(sizeof(dlistint_t));
-	ptr = malloc(sizeof(dlistint_t));
-	if (!new_node || !ptr)
+	if (!new_node)
 		return (NULL);
 
 	/* Create new node */
