4e6cb02bfddb7612202cd8c54b55987292d700a8
Alfred Tuva <muindetuva@gmail.com>
2021-05-28 13:57:37 +0000

    Remove unused variable 'value'

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,7 +11,6 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *temp;
-	char *value = NULL;
 	unsigned long int index;
 
 	index = key_index((const unsigned char *)key, ht->size);
