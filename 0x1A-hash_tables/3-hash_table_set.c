985bef2366b97cfee0916ce6971093c45b9b8049
Alfred Tuva <muindetuva@gmail.com>
2021-05-28 12:28:22 +0000

    Add task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,50 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_set - Adds an element to the hash table
+  * @ht: The hash table
+  * @key: The key
+  * @value: The value associated in key
+  *
+  * Return: 1 if succeeded 0 otherwise
+  */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned int long index;
+	hash_node_t *temp, *new_node;
+
+	if (key == NULL || ht == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	/* Create new node */
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+
+
+	while (temp != NULL)
+	{
+		if (strcmp(key, temp->key) == 0)
+		{
+			new_node->next = temp;
+			temp = new_node;
+			return (1);
+		}
+		temp = temp->next;
+	}
+	new_node->next = NULL;
+	temp = new_node;
+	return (1);
+}
+
+
+
+	
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -2,6 +2,7 @@
 #define HASH_TABLES
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Structures to be used */
 /**
