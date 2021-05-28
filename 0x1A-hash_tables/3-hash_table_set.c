90cf1efa5f69073153807b334d7dd35708a469cb
Alfred Tuva <muindetuva@gmail.com>
2021-05-28 12:59:12 +0000

    Set the value of next

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -40,7 +40,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		temp = temp->next;
 	}
-	new_node->next = NULL;
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
 }
