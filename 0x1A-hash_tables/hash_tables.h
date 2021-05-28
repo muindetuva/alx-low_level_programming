0df5e9866db0180c880d0ea137b51f8288fe13e0
Alfred Tuva <muindetuva@gmail.com>
2021-05-28 10:01:15 +0000

    Remove trailing spaces

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -14,9 +14,9 @@
  */
 typedef struct hash_node_s
 {
-     	char *key;
-     	char *value;
-     	struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -29,7 +29,7 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     	unsigned long int size;
+	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
 
